9442268ac89a80d96c58da57a0d31bac14970363
Baila3 <bailaly16@gmail.com>
2022-04-13 14:27:02 -0400

    net require + name + connect success

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
@@ -6,9 +7,15 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server" )
+  })
   conn.on('data', (data) => {
       console.log(data);
     })
+  conn.on('connect', () => {
+    conn.write('Name: VIN');
+  });
   return conn;
 };
 module.exports = {
