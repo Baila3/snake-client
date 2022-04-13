15515c6a9af333d6988b1941f922755b35a02599
Baila3 <bailaly16@gmail.com>
2022-04-13 15:40:56 -0400

    moving snake

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { consumers } = require("stream");
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
@@ -8,8 +9,14 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('connect', () => {
-    console.log("Successfully connected to game server" )
+    console.log("Successfully connected to game server" );
+    setInterval(() => {
+      conn.write('Move: up')
+      }, 500);
   })
+  // conn.on('connect', (data) => {
+    
+  // })-
   conn.on('data', (data) => {
       console.log(data);
     })
